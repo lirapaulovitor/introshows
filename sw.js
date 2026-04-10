b28cff0e2db35e388acd05b0c386852b3f1f50c4
lirapaulovitor <lira.paulovitor@gmail.com>
2026-04-10 18:58:23 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,38 @@
+var CACHE_NAME = 'intros-v1';
+var ASSETS = [
+  './',
+  './index.html',
+  './intro-album.mp3',
+  './intro-ficcao.mp3'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(
+        names.filter(function (n) { return n !== CACHE_NAME; })
+             .map(function (n) { return caches.delete(n); })
+      );
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
